b381a45bf51e7aac69fe907d2e9c4e8774c9fdbd
ViktorBentes <viktorbentes@gmail.com>
2022-12-26 19:04:43 -0300

    primeiro commit

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/pages/home/widgets/app_bar/mobile_app_bar.dart';
+import 'package:untitled/pages/home/widgets/app_bar/web_app_bar.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          appBar: constraints.maxWidth < 800 ? PreferredSize(child: MobileAppBar(), preferredSize: Size(double.infinity, 56),)
+              : PreferredSize(child: WebAppBar(), preferredSize: Size(double.infinity, 72)),
+              drawer: Drawer(),
+        );
+      },
+    );
+  }
+}
---
+++ lib/pages/home/widgets/app_bar/web_app_bar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class WebAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.black,
+      toolbarHeight: 72,
+      title: Row(
+        children: [
+          Text("Responsividade"),
+          const SizedBox(width: 32),
+          Expanded(child: Container()),
+          IconButton(onPressed: (){}, icon: Icon(Icons.shopping_cart)),
+          const SizedBox(width: 24),
+          SizedBox(
+            height: 38,
+            child: OutlinedButton(onPressed: () {}, child: Text("Fazer Login", style: TextStyle(color: Colors.white),),
+            style: OutlinedButton.styleFrom(side: BorderSide(color: Colors.white, width: 2)),
+            )
+          ),
+          const SizedBox(width: 8),
+          SizedBox(
+            height:40,
+            child: ElevatedButton(onPressed: () {}, child: Text("Cadastre-se", style: TextStyle(color: Colors.black),),
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+            )
+          )
+        ],
+      ),
+    );
+  }
+}
