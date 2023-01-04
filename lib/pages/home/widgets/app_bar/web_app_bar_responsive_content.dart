e53eb2cf56fae2d82a8cee2ad35ce0193aeeefb9
ViktorBentes <viktorbentes@gmail.com>
2023-01-04 11:18:31 -0300

    teste

--- lib/pages/home/widgets/app_bar/web_app_bar_responsive_content.dart
+++ lib/pages/home/widgets/app_bar/web_app_bar_responsive_content.dart
@@ -1,77 +1,64 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WebAppBarResponsiveContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final ButtonStyle outlineButtonStyle = OutlinedButton.styleFrom(
-      primary: Colors.white,
-      backgroundColor: Colors.black,
-      minimumSize: Size(88, 36),
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(2)),
-      ),
-    ).copyWith(
-      side: MaterialStateProperty.resolveWith<BorderSide>(
-        (Set<MaterialState> states) {
-          return BorderSide(
-            color: Colors.white,
-            width: 2,
+    return Expanded(
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          print(
+              "biggest ${constraints.biggest} smallest ${constraints.smallest}");
+
+          return Row(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 45,
+                  decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                      border: Border.all(color: Color(0xff757575))),
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 4),
+                      IconButton(
+                          onPressed: () {},
+                          color: Colors.grey[500],
+                          icon: Icon(Icons.search)),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Pesquise alguma coisa aqui",
+                              isCollapsed: true),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              if (constraints.maxWidth >= 400) ...[
+                SizedBox(width: 32),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Aprender"),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.black, onPrimary: Color(0xffffffff)),
+                ),
+              ],
+              if (constraints.maxWidth >= 500) ...[
+                SizedBox(width: 8),
+                ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Flutter"),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.black, onPrimary: Color(0xffffffff))),
+              ],
+            ],
           );
         },
       ),
-    );
-
-    final ButtonStyle elevatedButtonStyle = OutlinedButton.styleFrom(
-      primary: Colors.black,
-      backgroundColor: Colors.white,
-      minimumSize: Size(88, 36),
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(2)),
-      ),
-    ).copyWith(
-      side: MaterialStateProperty.resolveWith<BorderSide?>(
-        (Set<MaterialState> states) {
-          if (states.contains(MaterialState.pressed))
-            return BorderSide(
-              color: Colors.black,
-              width: 1,
-            );
-          return null; // Defer to the widget's default.
-        },
-      ),
-    );
-
-    return AppBar(
-      backgroundColor: Colors.black,
-      toolbarHeight: 72,
-      title: Row(
-        children: [
-          Text('Responsividade'),
-          const SizedBox(width: 32),
-          Expanded(child: Container()),
-          IconButton(icon: Icon(Icons.shopping_cart), onPressed: () {}),
-          const SizedBox(width: 24),
-          SizedBox(
-            height: 38,
-            child: OutlinedButton(
-              child: Text('Fazer login'),
-              style: outlineButtonStyle,
-              onPressed: () {},
-            ),
-          ),
-          const SizedBox(width: 8),
-          SizedBox(
-            height: 40,
-            child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Cadastre-se'),
-                style: elevatedButtonStyle),
-          )
-        ],
-      ),
-      centerTitle: true,
     );
   }
 }
